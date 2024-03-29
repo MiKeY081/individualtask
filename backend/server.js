5b0e93d1d838d31a996a939547136e2ad5ccccde
Mikie <kcmadhu081@gmail.com>
2024-03-29 23:47:38 +0545

    hi

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
-import { PrismaClient } from '@prisma/client'
-import express from "express"
-import cors from "cors"
-import morgan from "morgan"
-import { userRouter } from './routes/route.js'
-import { config } from 'dotenv'
-import cookieParser from 'cookie-parser';
-
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { userRouter } from "./routes/route.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 
 config();
-const prisma = new PrismaClient()
-const app = express()
-app.use(cors({
+const prisma = new PrismaClient();
+const app = express();
+app.use(
+  cors({
     origin: "https://individualtask.vercel.app",
     credentials: true,
     secure: true,
-}))
+  })
+);
 
 // app.use(cors({
 //     origin: "https://individualtask.vercel.app",
 //     credentials: true,
 //     secure: true,
 // }));
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
+app.use("/api/v1", userRouter);
 
-app.use("/api/v1", userRouter)
-
-const server = app.listen(3000)
+const server = app.listen("https://individualtask-backend.vercel.app/api/v1");
